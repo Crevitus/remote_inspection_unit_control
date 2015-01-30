9fe12e6cb1582a0aef23aafb208cced978f6dddd
vnikolova <vnikolova@hotmail.co.uk>
2015-01-30 15:06:05 +0000

    buttons n stuff

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -53,5 +53,13 @@ namespace remote_inspection_unit_control
 			}
 			
         }
+
+        private void devices_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        } 
     }
 }
