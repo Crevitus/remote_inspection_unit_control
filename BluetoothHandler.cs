ba40455d36bb9dd29513d96517eec563a30344c7
the_crevis <bcrevis@gmail.com>
2015-02-18 18:55:08 +0000

    bluetooth updates

--- BluetoothHandler.cs
+++ BluetoothHandler.cs
@@ -10,6 +10,7 @@ using InTheHand.Net.Sockets;
 using InTheHand.Net;
 using System.Windows;
 using System.Net.Sockets;
+using System.IO;
 
 
 namespace remote_inspection_unit_control
@@ -20,9 +21,25 @@ namespace remote_inspection_unit_control
         private static BluetoothDeviceInfo _selectedDevice;
         private static BluetoothClient _client = new BluetoothClient();
         private static NetworkStream _bluetoothStream;
+        private static bool _connected = false;
 
-        public static void selectDevice(string device)
+        public static bool Connected
         {
+            get { return _connected; }
+        }
+
+        public static void disconnect()
+        {
+            _bluetoothStream.Dispose();
+        }
+
+        public static bool selectDevice(string device)
+        {
+            if (_bluetoothStream != null)
+            {
+                _bluetoothStream.Close();
+                _client = new BluetoothClient();
+            }
             Guid _serviceGuid = new Guid("94f39d29-7d6d-437d-973b-fba39e49d4ee");
             _selectedDevice = _devicesInfo[device];
             try
@@ -34,12 +51,13 @@ namespace remote_inspection_unit_control
 
                 // get stream for send the data
                  _bluetoothStream = _client.GetStream();
+                 _connected = true;
             }
-            catch(Exception)
+            catch(SocketException)
             {
                 MessageBox.Show("Could not connect to device, please make sure it is in range and switched on.", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
+            return _connected;
         }
 
         public static async Task<List<String>> discoverAsync()
@@ -55,24 +73,29 @@ namespace remote_inspection_unit_control
             return items;
         }
 
-        public static async Task<bool> send(string content)
-        { 
-            //do not block UI thread
-            Task<bool> task = Task.Run(() =>
-            {    
+        public static void send(string content)
+        {
+            try
+            {
                 // if all is ok to send
-                if(_client.Connected && _bluetoothStream != null)
+                if (_client.Connected && _bluetoothStream != null)
                 {
                     // write the data in the stream
                     byte[] buffer = System.Text.Encoding.UTF8.GetBytes(content);
                     _bluetoothStream.Write(buffer, 0, buffer.Length);
                     _bluetoothStream.Flush();
                     //bluetoothStream.Close();
-                    return true;
+
                 }
-                return false;
-            });
-            return await task;
+                else
+                {
+                    throw new IOException();
+                }
+            }
+            catch(IOException)
+            {
+                MessageBox.Show("Failed to send message to device.", "Transmission Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private static void receive()
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace remote_inspection_unit_control
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool fullScreen = false;
+        bool _fullScreen = false;
+        bool _isMax = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -53,8 +54,16 @@ namespace remote_inspection_unit_control
 
         private void btnExitClick(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (MessageBox.Show("Drone is still connected, are you sure you want to exit?", "Exit?", MessageBoxButton.YesNo, MessageBoxImage.Question)
-                == MessageBoxResult.Yes)
+            if(BluetoothHandler.Connected)
+            {
+                if (MessageBox.Show("Drone is still connected, are you sure you want to exit? This will shutdown the drone.", "Exit?", MessageBoxButton.YesNo, MessageBoxImage.Question)
+                    == MessageBoxResult.Yes)
+                {
+                    BluetoothHandler.send("exit");
+                    Application.Current.Shutdown();
+                }
+            }
+            else
             {
                 Application.Current.Shutdown();
             }
@@ -70,12 +79,14 @@ namespace remote_inspection_unit_control
         	if(WindowState == WindowState.Normal)
 			{
 				WindowState = WindowState.Maximized;
+                _isMax = true;
 				btnMaximize.Content = "2";
 				btnMaximize.ToolTip = "Restore";
 			}
 			else
 			{
 				WindowState = WindowState.Normal;
+                _isMax = false;
 				btnMaximize.Content = "1";
 				btnMaximize.ToolTip = "Maximize";
 			}
@@ -90,56 +101,94 @@ namespace remote_inspection_unit_control
             (sender as Button).ContextMenu.IsOpen = true;
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void btnManual_Click(object sender, RoutedEventArgs e)
         {
-            await BluetoothHandler.send("shutdown");
+            BluetoothHandler.send("m");
         }
 
-        private void cbxDeviceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void cbxDeviceList_DropDownClosed(object sender, EventArgs e)
         {
-            string device = cbxDeviceList.SelectedItem.ToString();
-            BluetoothHandler.selectDevice(device);
+            if (cbxDeviceList.SelectedItem != null)
+            {
+                string device = cbxDeviceList.SelectedItem.ToString();
+                if (BluetoothHandler.selectDevice(device))
+                {
+                    lblConStatus.Content = "Connected";
+                    lblConStatus.Foreground = new SolidColorBrush(Colors.Green);
+                    btnDisconnect.IsEnabled = true;
+                }
+                else
+                {
+                    cbxDeviceList.Text = "-- Select Device --";
+                }
+            }
         }
 
         private void btnMediaFullScreen_Click(object sender, RoutedEventArgs e)
         {
-            if(!fullScreen)
-            {
-                layoutRoot.Children.Remove(gdMediaWrapper);
-                this.Background = new SolidColorBrush(Colors.Black);
-                this.Content = gdMediaWrapper;
-                this.MaxHeight = SystemParameters.MaximumWindowTrackHeight;
-                this.WindowState = WindowState.Maximized;
-            }
-            else
-            {
-                this.Content = layoutRoot;
-                layoutRoot.Children.Add(gdMediaWrapper);
-                this.Background = new SolidColorBrush(Colors.LightGray);
-                this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-                this.WindowState = WindowState.Normal;
-            }
-            fullScreen = !fullScreen;
+            fullScreen(gdMediaWrapper);
         }
         private void btnMapFullScreen_Click(object sender, RoutedEventArgs e)
         {
-            if (!fullScreen)
+            fullScreen(gdMapWrapper);
+        }
+
+        private void fullScreen(Grid control)
+        {
+            if (!_fullScreen)
             {
-                layoutRoot.Children.Remove(gdMapWrapper);
+                layoutRoot.Children.Remove(control);
                 this.Background = new SolidColorBrush(Colors.Black);
-                this.Content = gdMapWrapper;
-                this.MaxHeight = SystemParameters.MaximumWindowTrackHeight;
+                this.Content = control;
+                this.MaxHeight = 100000;
+                control.Margin = new Thickness(8);
+                this.WindowState = WindowState.Normal;
                 this.WindowState = WindowState.Maximized;
             }
             else
             {
                 this.Content = layoutRoot;
-                layoutRoot.Children.Add(gdMapWrapper);
+                layoutRoot.Children.Add(control);
                 this.Background = new SolidColorBrush(Colors.LightGray);
                 this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-                this.WindowState = WindowState.Normal;
+                if (!_isMax)
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.WindowState = WindowState.Maximized;
+                }
             }
-            fullScreen = !fullScreen;
+            _fullScreen = !_fullScreen;
+        }
+
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            BluetoothHandler.send("f");
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            BluetoothHandler.send("l");
+        }
+
+        private void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+            BluetoothHandler.send("r");
+        }
+
+        private void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            BluetoothHandler.disconnect();
+            lblConStatus.Content = "Disconnected";
+            lblConStatus.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            getDevices();
         }
     }
 }
